9c594b02a5a3e099cc729b02cb79163f086e9396
Andrew Theken <andrew@attr.io>
2023-08-13 16:39:43 +0000

    Add tailwindcss background process.

--- generator/Program.cs
+++ generator/Program.cs
@@ -1,3 +1,7 @@
+using System.Collections;
+using System.Diagnostics;
+using System.IO.Pipelines;
+
 namespace generator;
 
 record Settings(string Source, string Dest, bool Serve = false);
@@ -6,6 +10,8 @@ public static class Program
 {
     public static async Task<int> Main(string[] args)
     {
+        PreviewTailwind(args);
+
         return await Bootstrapper
           .Factory
           .CreateWeb(args)
@@ -14,6 +20,37 @@ public static class Program
           .SetOutputPath(new NormalizedPath("../site", PathKind.Relative))
           .AddPipeline<BrewPipeline>()
           .RunAsync();
+    }
+
+    private static void PreviewTailwind(string[] args)
+    {
+        if (args.Any(k => k == "preview"))
+        {
+            try
+            {
+                var proc = new Process();
+                proc.StartInfo.ArgumentList.AddRange("-w", "-m", "-o", "./input/public/main.css", "-i",
+                "./input/public/app.css", "-c", "./tailwind.config.js");
+
+                proc.StartInfo.FileName = "tailwindcss";
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+
+                proc.Start();
+
+                AppDomain.CurrentDomain.ProcessExit += (o, e) =>
+                {
+                    Console.WriteLine($"tailwind exit code {proc.ExitCode}");
+                    proc.Kill();
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Tailwindcss didn't launch: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                throw;
+            }
+        }
     }
 }
 
